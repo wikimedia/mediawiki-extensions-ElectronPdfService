f685bdf01384119dcdce5a618f443f2a0df384a0
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2016-09-29 15:59:55 +0200

    Override print-version link when Collection extension is installed
    When the Collection extension is installed it is moving the
    "printable version" link to the Print/export section.
    In that case we need to override it there.
    
    Bug: T146894
    Change-Id: If9cd3620a7da5e01064817da511209cb3cd67daa

--- ElectronPdfService.hooks.php
+++ ElectronPdfService.hooks.php
@@ -17,7 +17,9 @@ class ElectronPdfServiceHooks {
 
 		$specialPageTitle = SpecialPage::getTitleFor( 'ElectronPdf' );
 
+		// if the Collection extension is installed, modify their portlet
 		if ( array_key_exists( 'coll-print_export', $bar ) ) {
+			// TODO: don't add a new element to the sidebar, but reuse the "Download as PDF" link instead
 			$bar['coll-print_export'][] = [
 				'text' => $skin->msg( 'electronPdfService-sidebar-portlet-print-text' )->escaped(),
 				'id' => 'electron-print_pdf',
@@ -25,6 +27,15 @@ class ElectronPdfServiceHooks {
 					[ 'page' => $title->getPrefixedText() ]
 				)
 			];
+
+			// find the element for the print version and override it with the link to our SpecialPage
+			foreach ( $bar['coll-print_export'] as $index => $element ) {
+				if ( $element['id'] === 't-print' ) {
+					$bar['coll-print_export'][$index]['href'] = $specialPageTitle->getLocalURL(
+						[ 'page' => $title->getPrefixedText() ]
+					);
+				}
+			}
 		}
 
 		return true;
@@ -38,7 +49,8 @@ class ElectronPdfServiceHooks {
 
 		$specialPageTitle = SpecialPage::getTitleFor( 'ElectronPdf' );
 
-		if ( array_key_exists( 'print', $navUrls ) ) {
+		// if there's an element for a print version, override it with the link to our SpecialPage
+		if ( array_key_exists( 'print', $navUrls ) && $navUrls['print'] !== false ) {
 			$navUrls['print'] = [
 				'text' => $skin->msg( 'printableversion' )->text(),
 				'href' => $specialPageTitle->getLocalURL(
