3ca54d4d1c73a624a4504ba6a97b2347ed93b00e
addshore <addshorewiki@gmail.com>
2016-11-17 16:25:47 +0000

    Adjust to redirect directly to a RESTbase URL
    Bug: T150427
    Change-Id: I3273ca18aaaf97d47f57769c35e8deb133be5073

--- tests/browser/features/support/pages/selectionscreen_page.rb
+++ tests/browser/features/support/pages/selectionscreen_page.rb
@@ -2,7 +2,7 @@ class SelectionScreenPage
   include PageObject
 
   div(:selectionscreen_header, css: '.mw-electronPdfService-selection-header')
-  radio_button(:singlecolumn_selection, css: '.mw-electronPdfService-selection-form [value=download-electron-pdf]')
+  radio_button(:singlecolumn_selection, css: '.mw-electronPdfService-selection-form [value=redirect-to-electron]')
   radio_button(:twocolumn_selection, css: '.mw-electronPdfService-selection-form [value=redirect-to-collection]')
   button(:pdf_download_button, css: '.mw-electronPdfService-selection-form .oo-ui-buttonElement-button')
 end
