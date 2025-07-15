345f258f802b0ac5a742c88f67e63cab26c6f1f6
anterdc99 <xuminruixuan@live.cn>
2025-07-15 13:24:47 +0800

    ElectronPdfService: Translate special page aliases into Chinese
    Change-Id: Id0f3b0eae5c3bfc13ae5eb3182f41df11f08106a

--- ElectronPdfService.i18n.alias.php
+++ ElectronPdfService.i18n.alias.php
@@ -22,3 +22,18 @@ $specialPageAliases['az'] = [
 $specialPageAliases['ur'] = [
 	'DownloadAsPdf' => [ 'الیکٹران_پی_ڈی_ایف' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'DownloadAsPdf' => [ 'DownloadAsPdf' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'DownloadAsPdf' => [ '下载为PDF' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'DownloadAsPdf' => [ '下載為PDF' ],
+];
